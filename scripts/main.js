e068f6f7d77fb89a940f494c5097540b57f0edbc
Arnold Scott <arnvscott@gmail.com>
2020-10-15 17:17:42 +1000

    Added Mobile Menu transition

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,27 @@
+
+window.onscroll = function (){
+    scrollFunction();
+    // config.log ("Scrolling...");
+}
+
+
+
 function openNav () {
     document.getElementById("main-nav").style.width = "100%";
 }
 
 function closeNav () {
     document.getElementById("main-nav").style.width = "0"
+}
+
+function scrollFunction () {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("main-menu").classList.add("main-menu--active");
+        document.getElementById("main-menu__open-btn").classList.add("main-menu__open-btn--active");
+
+    } else {
+        document.getElementById("main-menu").classList.remove("main-menu--active");
+        document.getElementById("main-menu__open-btn").classList.remove("main-menu__open-btn--active");
+
+    }
 }
